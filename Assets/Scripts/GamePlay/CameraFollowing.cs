a3f15f38fb1e7f3c58ec3b0045c364d82e324ac5
WilliamNguyen <dungnguyen260103@gmail.com>
2023-04-24 11:53:24 +0700

    + add the scene,joystick,anim run idle

---
+++ Assets/Scripts/GamePlay/CameraFollowing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private float speed;
+    void Start()
+    {
+        speed = 5;   
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + new Vector3(0, 15, -15), Time.deltaTime * speed);
+        transform.transform.rotation = Quaternion.Euler(45, 0, 0);
+    }
+}
---
+++ Assets/Scripts/GamePlay/CircleAttack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleAttack : MonoBehaviour
+{
+    public int segments = 150;
+    public float radius = 10f;
+
+    [SerializeField] private LineRenderer line;
+
+    void Start()
+    {
+        line.positionCount = segments + 1;
+        line.useWorldSpace = false;
+        Draw();
+    }
+
+    void Draw()
+    {
+        float angle = 0f;
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            float x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            float y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+            line.SetPosition(i, new Vector3(x, y, 0f));
+
+            angle += (360f / segments);
+        }
+    }
+}
